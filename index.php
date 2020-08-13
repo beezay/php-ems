78518d4f3868c20caa2cfc089925a9ef08cd89a1
Beezay Bohora <bijay.okur@gmail.com>
2020-08-13 16:51:40 +0545

    UI Updated

--- index.php
+++ index.php
@@ -6,36 +6,45 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Beezay Networking</title>
+    <title>Employee Managemet System(EMS) </title>
 </head>
+
 <body>
 
-<div class="container">
-<h3>Hello!, Welcome to Beezay Networking</h3>
-<form action="" method="POST" >
-        <table>
-            <tr>
-            <td>Email</td>
-            <td><input type="email" name="u_email" required></td>
-            </tr>
-            <tr>
-            <td>Password</td>
-            <td><input type="password" name="u_pass" required></td>
-            </tr>
-            <td></td>
-            <td><input type="submit" name="u_login"></td>
-            </tr>
-        </table>
-        <h4>No Account</h4>
-        <button><a class="btn btn-success" href="register.php">Register</a></button>
-    </form>
-</div>
+    <div class="container">
+        <h3 class="display-4 text-success">Hello!, Welcome to Beezay EMS</h3>
+        <div class="panel panel-default">
+            <div class="panel-heading">Login</div>
+            <div class="panel-body">
+                <form action="" method="POST">
+                    <div class="form-group">
+                        <input type="text" class="form-control input-sm" name="u_email" placeholder="User Email"
+                            required>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control input-sm" name="u_pass" placeholder="User Password"
+                            required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-sm btn-success" name="u_login" value="LOGIN">
+                    </div>
+                    <p class="lead text-primary">No Account</p>
+                    <div class="form-group">
+                        <a class="btn btn-warning" href="register.php">Register</a>
+                    </div>
 
-<?php
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <?php
 
 if(isset($_POST['u_login'])){
     $u_email = $_POST['u_email'];
@@ -58,8 +67,9 @@ if(isset($_POST['u_login'])){
 }
 ?>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,38 +1,47 @@
 <?php require 'conn.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <title>Register</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Register on EMS</title>
 </head>
+
 <body>
 
-<div class="container">
-<form action="" method="POST" >
-        <table>
-            <tr>
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading">REGISTER</div>
+      <form action="" method="POST">
+        <table class="table table-bordered">
+          <tr>
             <td>Email</td>
             <td><input type="email" name="u_email" required></td>
-            </tr>
-            <tr>
+          </tr>
+          <tr>
             <td>Name</td>
             <td><input type="text" name="u_name" required></td>
-            </tr>
-            <tr>
+          </tr>
+          <tr>
             <td>Password</td>
             <td><input type="password" name="u_pass" required></td>
-            </tr>
-            <tr>
+          </tr>
+          <tr>
             <td></td>
-            <td><input type="submit" name="u_reg"></td>
-            </tr>
+            <td>
+              <input type="submit" class="btn btn-sm btn-success" name="u_reg">
+              <a class="btn btn-sm btn-danger" href="index.php">Back</a>
+            </td>
+          </tr>
         </table>
-    </form>
-</div>
+      </form>
+    </div>
+  </div>
 
-<?php
+
+  <?php
 
 if(isset($_POST['u_reg'])){
   $u_email = $_POST['u_email'];
@@ -55,8 +64,9 @@ if(isset($_POST['u_reg'])){
 
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
